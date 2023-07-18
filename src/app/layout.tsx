607afd028cdce15f6b8a136d12b560c55be3b27c
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2023-07-18 21:41:12 +0700

    video 17

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>Header Hỏi Dân IT</header>
+        {children}
+        <footer>footer</footer>
+      </body>
     </html>
   )
 }
