04be87a10134c9d590878adab9a20363edc9a712
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2023-07-18 21:58:28 +0700

    video 18

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 
 import Link from 'next/link';
+import x from '@/styles/app.module.css';
+import y from '@/styles/hoidanit.module.css';
 
 export default function Home() {
   return (
     <div>
       <ul>
-        <li >
+        <li className={x['red']} >
           <Link href={"/facebook"}>
-
-            Facebook
+            <span className={y['red']}>Facebook</span>
           </Link>
         </li>
         <li style={{ margin: "20px 0" }}>
