0a8c5aba498c165cfb4e4203ba2f048c0bf26f77
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2023-07-31 21:42:38 +0700

    video 26

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AppHeader from '@/components/app.header';
 import AppFooter from '@/components/app.footer';
 import Container from 'react-bootstrap/Container';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -27,6 +29,18 @@ export default function RootLayout({
         </Container>
 
         <AppFooter />
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </body>
     </html>
   )
--- src/components/create.modal.tsx
+++ src/components/create.modal.tsx
@@ -3,6 +3,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 interface IProps {
     showModalCreate: boolean;
@@ -17,7 +18,35 @@ function CreateModal(props: IProps) {
     const [content, setContent] = useState<string>("");
 
     const handleSubmit = () => {
-        console.log(">> check data form: ", title, author, content)
+
+        if (!title) {
+            toast.error("Not empty title !")
+            return;
+        }
+        if (!author) {
+            toast.error("Not empty author !")
+            return;
+        }
+
+        if (!content) {
+            toast.error("Not empty content !")
+            return;
+        }
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ title, author, content })
+        }).then(res => res.json())
+            .then(res => {
+                if (res) {
+                    toast.success("Create new blog succeed !");
+                    handleCloseModal();
+                }
+            });
+
     }
 
     const handleCloseModal = () => {
