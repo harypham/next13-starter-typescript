b5ad875e6d59ef05c01c05ff1be6525493b16936
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2023-08-01 22:25:45 +0700

    video 30

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,9 +1,22 @@
+'use client'
+import Link from 'next/link';
+import Card from 'react-bootstrap/Card';
 
 const ViewDetailBlog = ({ params }: { params: { id: string } }) => {
-    console.log(">> check id: ", params.id)
     return (
-        <div>
-            view detail with hoidanit = {params.id}
+        <div >
+            <div className='my-3'>
+                <Link href={"/blogs"}> Go Back</Link>
+            </div>
+            <Card className="text-center">
+                <Card.Header>Title</Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                        With supporting text below as a natural lead-in to additional content.
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer className="text-muted">author</Card.Footer>
+            </Card>
         </div>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AppHeader />
-        <Container>
+        <Container style={{ minHeight: 'calc(100vh - 106px)' }}>
           {children}
         </Container>
 
