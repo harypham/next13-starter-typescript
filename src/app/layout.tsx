fba2b003965c25b19120bab6faadd8e61f426470
anguyen0501 <20h1120185@ut.edu.vn>
2023-12-18 17:30:57 +0700

    header footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>Header</header>
+        {children}
+        <footer>Footer</footer>
+      </body>
     </html>
   )
 }
