555d1ab44fd48b556b6102bdd61358ce035dfd24
anguyen0501 <20h1120185@ut.edu.vn>
2023-12-21 22:54:27 +0700

    call api test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
-
+'use client'
 import Link from 'next/link'
 import x from '@/styles/app.module.css'
 import AppTable from '@/components/app.table'
+import { useEffect } from 'react';
+
 export default function Home() {
+
+  useEffect(() => {
+    const fetchData = async () => { 
+      const res = await fetch("http://localhost:8000/blogs");
+      const data = await res.json();
+      console.log("check data", data);
+    }
+    fetchData();
+  }, []);
+
+
   return (
     <div>
       <ul>
--- src/components/app.header.tsx
+++ src/components/app.header.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Link from 'next/link';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -9,12 +10,17 @@ function AppHeader() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand>
+          <Link href="/" className='navbar-brand'>
+            NEXTJS13 NTA
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Link href="/facebook" className='nav-link'>Facebook</Link>
+            <Link href="/youtube" className='nav-link'>Youtube</Link>
+            <Link href="/tiktok" className='nav-link'>Tiktok</Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
